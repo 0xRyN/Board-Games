4616d5007a71b9253f0407c2da0fa4b9047f6297
ilyas hamchaoui <ilyes.hamchaoui@gmail.com>
2023-11-26 17:31:09 +0100

    implementation of the pawn class for checkers

---
+++ Board-Games/Games/Checkers/Pieces/Pawn.cpp
@@ -0,0 +1,50 @@
+#include "Pawn.hpp"
+
+Pawn::Pawn(int posX, int posY, Color pieceColor)
+    : Piece(posX, posY, pieceColor) {
+}
+
+bool Pawn::canMove(int toX, int toY) const {
+    // Movement rules for pawn checkers :
+    // 1. Can move diagonally forward one square
+    // If statements are separated like this for optimization (short-circuiting)
+
+    // Check if the pawn is moving diagonally forward one square
+    if (abs(toX - x) != 1 || abs(toY - y) != 1) {
+        return false;
+    }
+
+    // White pawns can only move up the board
+    if (color == Color::White) {
+        if (toY <= y) {
+            return false;
+        }
+    }
+
+    // Black pawns can only move down the board
+    if (color == Color::Black) {
+        if (toY >= x) {
+            return false;
+        }
+    }
+
+    return true;
+    
+}
+
+
+
+// TODO: Why is there fromX and fromY?
+bool Pawn::canCapture(int toX, int toY) const {
+    // Capture rules for pawn checkers :
+    // 1. Can move diagonally forward two squares
+    // 2. Can move diagonally forward one square
+    // If statements are separated like this for optimization (short-circuiting)
+
+    // Check if the pawn is moving diagonally forward two square
+    if (abs(toX - x) != 2 || abs(toY - y) != 2) {
+        return false;
+    }
+}
+
+
