2186f1f9b3f4efac578b04433f59b3a58c7b4721
Rayan Al Azawi <ryanalaz11@gmail.com>
2023-12-29 18:17:17 +0100

    Queen is difficult

--- Board-Games/Games/Checkers/Pieces/Pawn/CheckersQueen.cpp
+++ Board-Games/Games/Checkers/Pieces/Pawn/CheckersQueen.cpp
@@ -1,5 +1,7 @@
 #include "CheckersQueen.hpp"
 
+#include <iostream>
+
 CheckersQueen::CheckersQueen(int posX, int posY, Color pieceColor)
     : Piece(posX, posY, pieceColor) {
     texturePath = (pieceColor == Color::White)
@@ -7,66 +9,68 @@ CheckersQueen::CheckersQueen(int posX, int posY, Color pieceColor)
                       : "assets/Pieces/Checkers/Queen/dark.png";
 }
 
-bool CheckersQueen::canMove(GameState& state, int toX, int toY) const {
-    // Check if the queen is moving diagonally
-    if (abs(toX - x) != abs(toY - y)) {
+bool isWithinBounds(int x, int y, int boardSize) {
+    if (x < 0 || x >= boardSize || y < 0 || y >= boardSize) {
         return false;
     }
-
-    // Check if there is a piece in the destination square
-    if (state.getTileAt(toX, toY).getPiece() != nullptr) {
-        return false;
-    }
-
     return true;
 }
 
 bool CheckersQueen::canCapture(GameState& state, int toX, int toY) const {
-    // Check if the queen is moving diagonally
-    if (abs(toX - x) != abs(toY - y)) {
-        return false;
+    return false;
+}
+
+bool CheckersQueen::canMove(GameState& state, int toX, int toY) const {
+    return false;
+}
+
+void CheckersQueen::findAllMoves(
+    GameState& state, int x, int y, std::vector<Move>& moves,
+    std::vector<Move>& path, std::vector<std::vector<bool>>& visited) const {
+
+    std::cout << "Initial position: (" << x << ", " << y << ")" << std::endl;
+    bool captureMade = false;
+    static const int dirs[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+
+    for (const auto& dir : dirs) {
+        int nextX = x + dir[0];
+        int nextY = y + dir[1];
+
+        if (isWithinBounds(nextX, nextY, state.getBoardSize()) &&
+            !visited[nextX][nextY]) {
+            Piece* nextPiece = state.getTileAt(nextX, nextY).getPiece();
+            if (nextPiece && nextPiece->getColor() != color) {
+                int jumpX = nextX + dir[0];
+                int jumpY = nextY + dir[1];
+                if (isWithinBounds(jumpX, jumpY, state.getBoardSize()) &&
+                    !state.getTileAt(jumpX, jumpY).getPiece()) {
+                    // Perform capture
+                    visited[nextX][nextY] = true;
+                    path.push_back(Move(x, y, jumpX, jumpY, true));
+                    findAllMoves(state, jumpX, jumpY, moves, path, visited);
+                    path.pop_back();
+                    visited[nextX][nextY] = false;
+                    captureMade = true;
+                }
+            }
+        }
     }
 
-    // Check if there is a piece in the destination square
-    if (state.getTileAt(toX, toY).getPiece() != nullptr) {
-        return false;
+    if (!captureMade && !path.empty()) {
+        // If no further capture and path is not empty, consider this path as a
+        // valid move sequence
+        moves.insert(moves.end(), path.begin(), path.end());
     }
-    // check if there is a piece adjacent to the destination square & if it is
-    // the opposite color
-    int xDir = (toX - x) / abs(toX - x);
-    int yDir = (toY - y) / abs(toY - y);
-    if (state.getTileAt(toX - xDir, toY - yDir).getPiece() == nullptr &&
-        state.getTileAt(toX - xDir, toY - yDir).getPiece()->getColor() !=
-            color) {
-        return false;
-    }
-    return true;
 }
 
 const std::vector<Move>*
 CheckersQueen::getAllAvailableMoves(GameState& state) const {
     std::vector<Move>* moves = new std::vector<Move>();
-    // run isValidMove for all possible tiles diagonally from the queen
+    std::vector<Move> path;
+    std::vector<std::vector<bool>> visited(
+        state.getBoardSize(), std::vector<bool>(state.getBoardSize(), false));
 
-    bool capture = false;
-    for (int i = 1; i < 8; i++) {
-        if (isValidMove(state, x + i, y + i)) {
-            capture = canCapture(state, x + i, y + i);
-            moves->push_back(Move(x, y, x + i, y + i, capture));
-        }
-        if (isValidMove(state, x - i, y + i)) {
-            capture = canCapture(state, x - i, y + i);
-            moves->push_back(Move(x, y, x - i, y + i, capture));
-        }
-        if (isValidMove(state, x + i, y - i)) {
-            capture = canCapture(state, x + i, y - i);
-            moves->push_back(Move(x, y, x + i, y - i, capture));
-        }
-        if (isValidMove(state, x - i, y - i)) {
-            capture = canCapture(state, x - i, y - i);
-            moves->push_back(Move(x, y, x - i, y - i, capture));
-        }
-    }
+    findAllMoves(state, x, y, *moves, path, visited);
 
     return moves;
 }
--- Board-Games/Games/Checkers/Pieces/Pawn/CheckersQueen.hpp
+++ Board-Games/Games/Checkers/Pieces/Pawn/CheckersQueen.hpp
@@ -3,16 +3,18 @@
 
 #include "Board-Games/Piece.hpp"
 
-class CheckersQueen : public Piece{
-private:
-
-public:
+class CheckersQueen : public Piece {
+  private:
+  public:
     CheckersQueen(int posX, int posY, Color pieceColor);
-    
+
     bool canCapture(GameState& state, int toX, int toY) const override;
     bool canMove(GameState& state, int toX, int toY) const override;
+    void findAllMoves(GameState& state, int x, int y, std::vector<Move>& moves,
+                      std::vector<Move>& path,
+                      std::vector<std::vector<bool>>& visited) const;
     const std::vector<Move>*
     getAllAvailableMoves(GameState& state) const override;
 };
- 
+
 #endif // !CHECKERSQUEEN_HPP
