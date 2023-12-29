79f674a0b5bb7df5113969cf7aaac9b1c27941ad
ilyas hamchaoui <ilyes.hamchaoui@gmail.com>
2023-12-29 17:46:44 +0100

    modifications of getAvailableMoves() method

--- Board-Games/Games/Checkers/Pieces/Pawn/CheckersQueen.cpp
+++ Board-Games/Games/Checkers/Pieces/Pawn/CheckersQueen.cpp
@@ -24,44 +24,46 @@ bool CheckersQueen::canMove(GameState& state, int toX, int toY) const {
 const std::vector<Move>*
 CheckersQueen::getAllAvailableMoves(GameState& state) const {
     std::vector<Move>* moves = new std::vector<Move>();
-    int xDir = (state.getCurrentPlayer()->getColor() == Color::White) ? 1 : -1;
-    int yDir = (state.getCurrentPlayer()->getColor() == Color::White) ? 1 : -1;
-
-    // Check if the queen can move diagonally
-    for (int i = 1; i < state.getBoardSize(); i++) {
-        if (x + i * xDir >= state.getBoardSize() || x + i * xDir < 0 ||
-            y + i * yDir >= state.getBoardSize() || y + i * yDir < 0) {
-            break;
+    //run isValidMove for all possible tiles diagonally from the queen
+    
+    bool capture = false;
+    for (int i = 1; i < 8; i++) {
+        if (isValidMove(state, x + i, y + i)) {
+            capture = canCapture(state, x + i, y + i);
+            moves->push_back(Move(x, y, x + i, y + i,capture));
         }
-        if (canMove(state, x + i * xDir, y + i * yDir)) {
-            moves->push_back(Move(x, y, x + i * xDir, y + i * yDir , false));
-        } else {
-            break;
+        if (isValidMove(state, x - i, y + i)) {
+            capture = canCapture(state, x - i, y + i);
+            moves->push_back(Move(x, y, x - i, y + i,capture));
         }
-    }
-
-    // Check if the queen can capture diagonally
-    for (int i = 1; i < state.getBoardSize(); i++) {
-        if (x + i * xDir >= state.getBoardSize() || x + i * xDir < 0 ||
-            y + i * yDir >= state.getBoardSize() || y + i * yDir < 0) {
-            break;
+        if (isValidMove(state, x + i, y - i)) {
+            capture = canCapture(state, x + i, y - i);
+            moves->push_back(Move(x, y, x + i, y - i ,capture));
         }
-        if (canCapture(state, x + i * xDir, y + i * yDir)) {
-            moves->push_back(Move(x, y, x + i * xDir, y + i * yDir, true));
-        } else {
-            break;
+        if (isValidMove(state, x - i, y - i)) {
+            capture = canCapture(state, x - i, y - i);
+            moves->push_back(Move(x, y, x - i, y - i, capture));
         }
     }
-
+    
     return moves;
     
 }
 
 bool CheckersQueen::canCapture(GameState& state, int toX, int toY) const {
-        //check if there is a piece adjacent to the destination square
+        //Check if the queen is moving diagonally
+    if (abs(toX - x) != abs(toY - y)) {
+        return false;
+    }
+
+    // Check if there is a piece in the destination square
+    if (state.getTileAt(toX, toY).getPiece() != nullptr) {
+        return false;
+    }
+        //check if there is a piece adjacent to the destination square & if it is the opposite color
     int xDir = (toX - x) / abs(toX - x);
     int yDir = (toY - y) / abs(toY - y);
-    if (state.getTileAt(toX - xDir, toY - yDir).getPiece() == nullptr) {
+    if (state.getTileAt(toX - xDir, toY - yDir).getPiece() == nullptr && state.getTileAt(toX - xDir, toY - yDir).getPiece()->getColor() != color) {
         return false;
     }
     return true;
